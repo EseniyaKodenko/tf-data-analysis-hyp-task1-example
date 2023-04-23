989c044c7dc8af5fea01637ad2d790e0c81a17d9
EseniyaKodenko <130905665+EseniyaKodenko@users.noreply.github.com>
2023-04-23 18:08:10 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,16 @@
 import pandas as pd
 import numpy as np
+from statsmodels.stats.weightstats import ztest
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 1019285902 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    x = [1] * x_success + [0] * (x_cnt - x_success)
+    y = [1] * y_success + [0] * (y_cnt - y_success)
+    pvalue = ztest(y, x, alternative="larger")[1]
+    ans = True if pvalue < 0.06 else False
+    return ans # Ваш ответ, True или False
