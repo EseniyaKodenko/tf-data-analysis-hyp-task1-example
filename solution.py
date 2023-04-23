9fed26bbfb0ab7eb6ab7a079115e65147362bacf
EseniyaKodenko <130905665+EseniyaKodenko@users.noreply.github.com>
2023-04-23 19:08:00 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -9,8 +9,8 @@ def solution(x_success: int,
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    x = [1] * x_success + [0] * (x_cnt - x_success)
-    y = [1] * y_success + [0] * (y_cnt - y_success)
+    x = [1] * int(x_success) + [0] * int(x_cnt - x_success)
+    y = [1] * int(y_success) + [0] * int(y_cnt - y_success)
     pvalue = ztest(y, x, alternative="larger")[1]
     ans = True if pvalue < 0.06 else False
     return ans # Ваш ответ, True или False
